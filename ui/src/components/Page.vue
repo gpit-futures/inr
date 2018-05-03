1f3eb8ec73b7b1d21c8d0b7105a59b6e600f4c25
Adrian Dickin <adrian.dickin@answerconsulting.com>
2018-05-03 08:10:18 +0100

    apply wireframe structure

--- ui/src/components/Page.vue
+++ ui/src/components/Page.vue
@@ -4,58 +4,56 @@
       <v-layout row align-baseline class="pa-2">
          <v-flex xs4>
          </v-flex>
-         <v-flex xs3 class="display-1 text-xs-center">
+         <v-flex xs4 class="headline text-xs-center">
            INR Module
          </v-flex>
-         <v-flex xs3>
-           <b>Dr E. Taylor</b>
-         </v-flex>
-         <v-flex xs2 class="text-xs-right">
-           26-April-2018
+         <v-flex xs4>
+           <v-layout row justify-space-between>
+             <b>Dr E. Taylor</b>
+             <span>26-April-2018</span>
+           </v-layout>
          </v-flex>
       </v-layout>
-      <v-layout row class="purple darken-1 white--text pl-2 pr-2 pt-3 pb-3">
-        <v-flex xs3>
-          <span class="body-1">SMITH, John (Mr)</span>
-        </v-flex>
-        <v-flex xs3>
+      <v-layout row class="purple darken-1 white--text pl-2 pr-2 pt-3 pb-3 justify-space-between">
+        <span class="body-1">SMITH, John (Mr)</span>
+        <div>
           <span class="caption">Born:</span>
           <span class="body-1">01-Aug-1975 (43y)</span>
-        </v-flex>
-        <v-flex xs2>
+        </div>
+        <div>
           <span class="caption">Gender:</span>
           <span class="body-1">Male</span>
-        </v-flex>
-        <v-flex xs2>
+        </div>
+        <div>
           <span class="caption">NHS Number:</span>
           <span class="body-1">111 999 0000</span>
-        </v-flex>
-        <v-flex xs2>
+        </div>
+        <div>
           <span class="caption">GP Practice:</span>
           <span class="body-1">AB0011</span>
-        </v-flex>
+        </div>
       </v-layout>
-      <v-layout row class="pa-2">
-        <v-flex xs3>
+      <v-layout row class="pa-2" justify-space-between>
+        <div>
           <span class="caption">Address:</span>
           <span class="body-1">1 Kings Road, Leeds, LS11 5AA</span>
-        </v-flex>
-        <v-flex xs3>
+        </div>
+        <div>
           <span class="caption">Diagnosis:</span>
           <span class="body-1">Atrial fibrillation</span>
-        </v-flex>
-        <v-flex xs2>
+        </div>
+        <div>
           <span class="caption">Drug:</span>
           <span class="body-1">Warfarin</span>
-        </v-flex>
-        <v-flex xs2>
+        </div>
+        <div>
           <span class="caption">Target INR:</span>
           <span class="body-1">2.2</span>
-        </v-flex>
-        <v-flex xs2>
+        </div>
+        <div>
           <span class="caption">Date:</span>
           <span class="body-1">Indefinite</span>
-        </v-flex>
+        </div>
       </v-layout>
     </v-card>
 
--- ui/src/components/TreatmentPlans.vue
+++ ui/src/components/TreatmentPlans.vue
@@ -1,32 +1,42 @@
 <template>
   <div>
-    <v-card flat class="mt-3 pa-2">
+    <v-card flat class="mt-3 px-2 pb-2">
       <v-layout row align-baseline>
-      <span class="caption">Treatment Plan:</span>
-      <v-select
-          :items="planDates"
-          v-model="selectedPlanDate"
-          label="Select"
-          single-line
-          hide-details
-          class="date-select d-inline-block ml-1"
-        ></v-select>
+        <span class="caption">Treatment Plan:</span>
+        <v-select
+            :items="planDates"
+            v-model="selectedPlanDate"
+            label="Select"
+            single-line
+            hide-details
+            class="date-select d-inline-block ml-1"
+          ></v-select>
       </v-layout>
     </v-card>
-    <v-data-table
-        :headers="headers"
-        :items="items"
-        hide-actions
-        class="elevation-1">
-      <template slot="items" slot-scope="data">
-        <td>{{ data.item.testDate }}</td>
-        <td>{{ data.item.inr }}</td>
-        <td>{{ data.item.dose }}</td>
-        <td>{{ data.item.reviewDays }}</td>
-        <td>{{ data.item.nextTestDate }}</td>
-        <td><a href='#'>Add Comment</a></td>
-      </template>
-    </v-data-table>
+
+    <table class="tabular">
+      <thead>
+        <tr class="purple darken-1 white--text">
+          <th>Test Date</th>
+          <th>INR</th>
+          <th>Dose (mg/day)</th>
+          <th>Review days</th>
+          <th>Next Test Date</th>
+          <th>Comments</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="row in items" :key="row.id">
+          <td>{{row.testDate}}</td>
+          <td>{{row.inr}}</td>
+          <td>{{row.dose}}</td>
+          <td>{{row.reviewDays}}</td>
+          <td>{{row.nextTestDate}}</td>
+          <td><a href='#'>Add Comment</a></td>
+        </tr>
+      </tbody>
+    </table>
+    <v-btn color="primary">Add New INR <v-icon right>playlist_add</v-icon></v-btn>
   </div>
 </template>
 
@@ -45,8 +55,8 @@ export default {
         {text: 'Next Test Date', value: 'nextTestDate'},
         {text: 'Comments', sortable: false}],
       items: [
-        {testDate: '14-April-2017', inr: '2.2', dose: '2.3', reviewDays: 14, nextTestDate: '01-May-2017'},
-        {testDate: '01-May-2017', inr: '2.2', dose: '2.4', reviewDays: 21, nextTestDate: '14-May-2017'}]
+        {id: 1, testDate: '14-April-2017', inr: '2.2', dose: '2.3', reviewDays: 14, nextTestDate: '01-May-2017'},
+        {id: 2, testDate: '01-May-2017', inr: '2.2', dose: '2.4', reviewDays: 21, nextTestDate: '14-May-2017'}]
     }
   }
 }
@@ -55,5 +65,20 @@ export default {
 <style scoped>
 .date-select {
   max-width: 160px;
+}
+.tabular {
+  width: 100%;
+  border-collapse: collapse
+}
+.tabular thead tr {
+  text-align: center;
+}
+.tabular tbody tr {
+  text-align: center;
+}
+.tabular th,
+.tabular td {
+  font-weight: normal;
+  padding: 4px;
 }
 </style>
