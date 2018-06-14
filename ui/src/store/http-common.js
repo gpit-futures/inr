5f558e68a7c45220d6142b8705f49bc95817f76f
Daniel Nuttall <danielnuttall@answerdigital.com>
2018-06-14 12:49:55 +0100

    Change configuration to point inr front-end to deployed inr back-end.

--- ui/src/store/http-common.js
+++ ui/src/store/http-common.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const HTTP = axios.create({
-  baseURL: 'http://localhost:8082/inr/'
+  baseURL: 'http://ec2-35-177-96-55.eu-west-2.compute.amazonaws.com:8082/inr/'
 })
